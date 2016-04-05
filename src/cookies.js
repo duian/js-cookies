d11244952d96fdc2baf0e98f8a9369449a564035
duian <zhouqiao0418@gmail.com>
2016-04-05 11:55:06 +0800

    feat: optimize code with eslint

--- src/cookies.js
+++ src/cookies.js
@@ -2,46 +2,63 @@ const encode = (val) => {
   try {
     return encodeURIComponent(val);
   } catch (e) {
-    console.error(`error encode %o`);
+    console.error('error encode %o');
   }
+  return null;
 };
 
 const decode = (val) => {
   try {
-    return decodeURIComponent(val)
-  } catch(err) {
-    console.error(`error decode %o`);
+    return decodeURIComponent(val);
+  } catch (err) {
+    console.error('error decode %o');
   }
+  return null;
 };
 
-cookies = {
+const handleSkey = (sKey) => encode(sKey).replace(/[\-\.\+\*]/g, '\\$&');
+
+const Cookies = {
   getItem(sKey) {
     if (!sKey) { return null; }
-    return decode(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encode(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    return (
+      decode(
+        document.cookie
+        .replace(
+          new RegExp(`(?:(?:^|.*;)\\s*${handleSkey(sKey)}\\s*\\=\\s*([^;]*).*$)|^.*$`)
+        , '$1')
+      ) || null
+    );
   },
 
   setItem(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
     if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-    let sExpires = "";
+    let sExpires = '';
     if (vEnd) {
       switch (vEnd.constructor) {
         case Number:
-          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+          if (vEnd === Infinity) {
+            sExpires = '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+          } else {
+            sExpires = `; max-age=${vEnd}`;
+          }
           break;
         case String:
-          sExpires = "; expires=" + vEnd;
+          sExpires = `; expires=${vEnd}`;
           break;
         case Date:
-          sExpires = "; expires=" + vEnd.toUTCString();
+          sExpires = `; expires=${vEnd.toUTCString()}`;
+          break;
+        default:
           break;
       }
     }
     document.cookie = [
-      encode(sKey), "=", encode(sValue),
+      encode(sKey), '=', encode(sValue),
       sExpires,
-      (sDomain ? "; domain=" + sDomain : ""),
-      (sPath ? "; path=" + sPath : ""),
-      (bSecure ? "; secure" : "")
+      (sDomain ? `; domain=${sDomain}` : ''),
+      (sPath ? `; path=${sPath}` : ''),
+      (bSecure ? '; secure' : ''),
     ].join('');
     return true;
   },
@@ -49,23 +66,27 @@ cookies = {
   removeItem(sKey, sPath, sDomain) {
     if (!this.hasItem(sKey)) { return false; }
     document.cookie = [
-      encode(sKey), "=; expires=Thu, 01 Jan 1970 00:00:00 GMT",
-      (sDomain ? "; domain=" + sDomain : ""),
-      (sPath ? "; path=" + sPath : "")
+      encode(sKey), '=; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+      (sDomain ? `; domain=${sDomain}` : ''),
+      (sPath ? `; path=${sPath}` : ''),
     ].join('');
     return true;
   },
 
   hasItem(sKey) {
     if (!sKey) { return false; }
-    return (new RegExp("(?:^|;\\s*)" + encode(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    return (
+      new RegExp(`(?:^|;\\s*)${encode(sKey).replace(/[\-\.\+\*]/g, '\\$&')}\\s*\\=`)
+    ).test(document.cookie);
   },
 
   keys() {
-    let aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+    let aKeys = document.cookie
+    .replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, '')
+    .split(/\s*(?:\=[^;]*)?;\s*/);
     aKeys = aKeys.map((key) => decode(key));
     return aKeys;
-  }
+  },
 };
 
-export default cookies;
+export default Cookies;
